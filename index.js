bbedb9ec8b4cdde46a896c73aa0e9aa48ac4176e
Hunter Jansen <rawkamatic@gmail.com>
2017-10-08 10:45:00 -0400

    Add persistence between sessions Separate the code out a bit to make future expansion a little easier Fire events on page load if linkedin

--- index.js
+++ index.js
@@ -1,149 +1,149 @@
-document.addEventListener('DOMContentLoaded', function() {
-  $('#clear-photos').off().on('click',function(){
-    // $(this).text(function(i, text){
-    //   return text === "Clear Photos" ? "Reset Photos" : "Clear Photos";
-    // });
-    chrome.tabs.executeScript({
-      code: `
-        var prevStyle = document.getElementById('BIAS_LINKEDIN');
+const StorageArea = chrome.storage.local
+const KEYS = ['linkedinPhotos', 'linkedinNames']
 
-        if (prevStyle) {
-          prevStyle.parentNode.removeChild(prevStyle);
-        } else {
-          const style = document.createElement('style');
-          style.id = 'BIAS_LINKEDIN';
+const setStorageValue = (val, callback) => {
+  StorageArea.set(val, function() {
+    if (callback) {
+      callback()
+    }
+  })
+}
 
-          document.body.appendChild(style);
+const getStoredSettings = async () => {
+  return new Promise(function(resolve, reject) {
+    StorageArea.get(KEYS, values => {
+      resolve(values)
+    })
+  })
+}
 
-          const obfuscate = [
-            "span.full-name, a[href^='https://www.linkedin.com/profile'], #sticky-rail * { color: black !important; background-color: black !important; }",
+const injectScripts = () => {
+  return new Promise(function(resolve, reject) {
+    // TODO - Check if page is linkedin
+    chrome.tabs.executeScript(
+      {
+        file: 'page-scripts/linkedin.js'
+      },
+      () => {
+        resolve()
+      }
+    )
+  })
+}
 
-            "img { opacity: 0.5; -webkit-filter: blur(50px) !important; filter: blur(50px) !important; }",
+// When a tab finishes loading; hide stuff if applicable
+// Todo - inject scripts on page start if valid url
+chrome.tabs.onUpdated.addListener(function(tabId, info) {
+  if (info.status === 'complete') {
+    chrome.tabs.get(tabId, async tab => {
+      if (tab.url.includes('linkedin')) {
+        let settings = await getStoredSettings()
+        await injectScripts()
 
-            "#aux > div.insights > h3 { color: black !important; background-color: black !important; }",
-
-            "#aux > div.insights > h3::after { content: ''; text-indent: 0; display: block; line-height: initial; }",
-
-            "[id^='control_gen_'] > div.header > h3 { color: black !important; background-color: black !important; }",
-
-            "[id^='control_gen_'] > div.header > h3::after { content: ''; text-indent: 0; display: block; line-height: initial; }",
-
-            "#in-common > svg > circle[fill^='url('] { fill-opacity: 0 !important; fill: black !important; }"
-          ];
-
-          obfuscate.forEach((r, i) => style.sheet.insertRule(r, i));
+        if (settings.linkedinPhotos) {
+          chrome.tabs.executeScript({
+            code: `
+              obscureLinkedinPhotos()`
+          })
         }
-      `
-    });
-  });
 
-    $('#clear-names').off().on('click',function(){
-      // $(this).text(function(i, text){
-      //   return text === "Clear Names" ? "Reset Names" : "Clear Names";
-      // });
+        if (settings.linkedinNames) {
+          chrome.tabs.executeScript({
+            code: `
+              obscureLinkedinNames()`
+          })
+        }
+      }
+    })
+  }
+})
+
+document.addEventListener('DOMContentLoaded', async () => {
+  let settings = await getStoredSettings()
+
+  // TODO - remove jQuery dependency
+  $('#clear-photos')
+    .off()
+    .on('click', function() {
+      settings.linkedinPhotos = !settings.linkedinPhotos
+      setStorageValue({ linkedinPhotos: settings.linkedinPhotos })
+
       chrome.tabs.executeScript({
         code: `
-          var prevStyle = document.getElementById('BIAS_NAMES_LINKEDIN');
+        if (${settings.linkedinPhotos}) {
+          obscureLinkedinPhotos()
+        } else {
+          resetLinkedinPhotos()
+        }
+      `
+      })
+    })
 
-          if (prevStyle) {
-            prevStyle.parentNode.removeChild(prevStyle);
-          } else {
-            const style = document.createElement('style');
-            style.id = 'BIAS_NAMES_LINKEDIN';
+  $('#clear-names')
+    .off()
+    .on('click', function() {
+      settings.linkedinNames = !settings.linkedinNames
+      setStorageValue({ linkedinNames: settings.linkedinNames })
 
-            document.body.appendChild(style);
-
-            const nameObfuscate = [
-              '[data-control-name="identity_welcome_message"] { color: white !important; background-color: white !important; }',
-              '.neptune-grid > .launchpad__title { opacity: 0; }',
-              '[data-control-name="actor"] > h3 >  span:first-child { visibility: hidden;}',
-              '[data-control-name="actor"] > h3 >  span:first-child:before { content: "Link to Profile"; visibility: visible;}',
-              'span[class*="-name"] { visibility: hidden !important; }',
-              'span[class*="-name"]:before { content: "Link to Profile"; visibility: visible !important; }',
-              'span[class*="__name"] { visibility: hidden !important; }',
-              'h3[class*="__name"] { visibility: hidden !important; }',
-              'h3[class*="-name"] { visibility: hidden !important; }',
-              'a[class*="name"] { visibility: hidden !important; }',
-              'h3[class*="__name"]:before { content: "Link to Profile"; visibility: visible !important; }',
-              'a[class*="name"]:before { content: "Link to Profile"; visibility: visible !important; }',
-              '[class*="person-info__shared"] { opacity: 0.5; -webkit-filter: blur(50px) !important; filter: blur(50px) !important; }',
-              '.msg-conversation-card__message-snippet-body { opacity: 0; }',
-              '.profile-rail-card__actor-link > p { visibility: hidden; }',
-              '.pv-contact-info__card-sub-heading { opacity: 0; }',
-              '.pv-entity__summary-info > p { visibility: hidden; }',
-              '.entity-hovercard { opacity:0 !important;}',
-              '[data-control-name="edit_endorsements"] { visibility: hidden; }',
-              '[data-control-name="update_topbar_actor"] {visibility: hidden;}',
-              '.pv-recent-activity-section__card-heading {visibility: hidden}',
-              '[data-control-name="update_topbar_actor"]:before {content: "Link to Profile"; visibility: visible;}',
-              '[data-control-name="topcard"] h2 { visibility:hidden !important; }',
-              '[data-control-name="topcard"] h2:before { content: "Link to Profile"; visibility:visible !important; }',
-              '.pv-top-card-section__name:before {content : "" !important;}',
-              '.pv-top-card-section__name {visibility: hidden !important;}',
-              'span[class*="school"] { visibility: visible !important; }',
-              'span[class*="school"]:before { content: "" !important; }',
-              'span[class*="skill"]:before { content: "" !important; }',
-              'span[class*="skill"] { visibility: visible !important; }',
-              'span[class*="degree"]:before { content: "" !important; }',
-              'span[class*="degree"] { visibility: visible !important; }',
-            ];
-
-
-            nameObfuscate.forEach((r,i) => style.sheet.insertRule(r,i));
-          }
+      chrome.tabs.executeScript({
+        code: `
+        if (${settings.linkedinNames}) {
+          obscureLinkedinNames()
+        } else {
+          resetLinkedinNames()
+        }
         `
-      });
-    });
-});
+      })
+    })
+})
 
-
-
+// TODO -- Can we remove this? What's this for?
 // chrome.browserAction.onClicked.addListener(tab => {
-  // chrome.tabs.executeScript({
-  //   code: `
-  //     var prevStyle = document.getElementById('BIAS_LINKEDIN');
-  //
-  //     if (prevStyle) {
-  //       prevStyle.parentNode.removeChild(prevStyle);
-  //     } else {
-  //       const style = document.createElement('style');
-  //       style.id = 'BIAS_LINKEDIN';
-  //
-  //       document.body.appendChild(style);
-  //
-  //       const obfuscate = [
-  //         "span.full-name, a[href^='https://www.linkedin.com/profile'], #sticky-rail * { color: black !important; background-color: black !important; }",
-  //
-  //         "img { opacity: 0.5; -webkit-filter: blur(50px) !important; filter: blur(50px) !important; }",
-  //
-  //         "#aux > div.insights > h3 { color: black !important; background-color: black !important; }",
-  //
-  //         "#aux > div.insights > h3::after { content: ''; text-indent: 0; display: block; line-height: initial; }",
-  //
-  //         "[id^='control_gen_'] > div.header > h3 { color: black !important; background-color: black !important; }",
-  //
-  //         "[id^='control_gen_'] > div.header > h3::after { content: ''; text-indent: 0; display: block; line-height: initial; }",
-  //
-  //         "#in-common > svg > circle[fill^='url('] { fill-opacity: 0 !important; fill: black !important; }"
-  //       ];
-  //
-  //       const nameObfuscate = [
-  //         '[data-control-name="identity_welcome_message"] { color: white !important; background-color: white !important; }',
-  //         '.neptune-grid > .launchpad__title { opacity: 0; }',
-  //         '[data-control-name="actor"] > h3 >  span { color: white !important; background-color: white !important; }',
-  //         '.hoverable-link-text { display: none; }',
-  //         '[class*="name"] { display: none !important; }',
-  //         '[class*="person-info__shared"] { opacity: 0.5; -webkit-filter: blur(50px) !important; filter: blur(50px) !important; }',
-  //         '.msg-conversation-card__message-snippet-body { opacity: 0; }',
-  //         '.profile-rail-card__actor-link > p { display: none; }',
-  //         '.pv-contact-info__card-sub-heading { opacity: 0; }',
-  //         '.pv-entity__summary-info > p { display: none; }',
-  //         '[data-control-name="edit_endorsements"] { visibility: hidden; }'
-  //       ];
-  //
-  //       obfuscate.forEach((r, i) => style.sheet.insertRule(r, i));
-  //       nameObfuscate.forEach((r,i) => style.sheet.insertRule(r,i));
-  //     }
-  //   `
-  // })
+// chrome.tabs.executeScript({
+//   code: `
+//     var prevStyle = document.getElementById('BIAS_LINKEDIN');
+//
+//     if (prevStyle) {
+//       prevStyle.parentNode.removeChild(prevStyle);
+//     } else {
+//       const style = document.createElement('style');
+//       style.id = 'BIAS_LINKEDIN';
+//
+//       document.body.appendChild(style);
+//
+//       const obfuscate = [
+//         "span.full-name, a[href^='https://www.linkedin.com/profile'], #sticky-rail * { color: black !important; background-color: black !important; }",
+//
+//         "img { opacity: 0.5; -webkit-filter: blur(50px) !important; filter: blur(50px) !important; }",
+//
+//         "#aux > div.insights > h3 { color: black !important; background-color: black !important; }",
+//
+//         "#aux > div.insights > h3::after { content: ''; text-indent: 0; display: block; line-height: initial; }",
+//
+//         "[id^='control_gen_'] > div.header > h3 { color: black !important; background-color: black !important; }",
+//
+//         "[id^='control_gen_'] > div.header > h3::after { content: ''; text-indent: 0; display: block; line-height: initial; }",
+//
+//         "#in-common > svg > circle[fill^='url('] { fill-opacity: 0 !important; fill: black !important; }"
+//       ];
+//
+//       const nameObfuscate = [
+//         '[data-control-name="identity_welcome_message"] { color: white !important; background-color: white !important; }',
+//         '.neptune-grid > .launchpad__title { opacity: 0; }',
+//         '[data-control-name="actor"] > h3 >  span { color: white !important; background-color: white !important; }',
+//         '.hoverable-link-text { display: none; }',
+//         '[class*="name"] { display: none !important; }',
+//         '[class*="person-info__shared"] { opacity: 0.5; -webkit-filter: blur(50px) !important; filter: blur(50px) !important; }',
+//         '.msg-conversation-card__message-snippet-body { opacity: 0; }',
+//         '.profile-rail-card__actor-link > p { display: none; }',
+//         '.pv-contact-info__card-sub-heading { opacity: 0; }',
+//         '.pv-entity__summary-info > p { display: none; }',
+//         '[data-control-name="edit_endorsements"] { visibility: hidden; }'
+//       ];
+//
+//       obfuscate.forEach((r, i) => style.sheet.insertRule(r, i));
+//       nameObfuscate.forEach((r,i) => style.sheet.insertRule(r,i));
+//     }
+//   `
+// })
 // });
